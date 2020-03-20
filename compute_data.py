0761e08c78e957f59552b60eaf7fd1ff76a4ecc2
Kyle L <likyle001@gmail.com>
2020-03-20 13:48:14 -0400

    modified compute_data

--- compute_data.py
+++ compute_data.py
@@ -16,7 +16,7 @@ class stateData:
         self.popDensity = popDensity
     
     def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.startDate, self.popDensity)
+        return "%s, %s, %s, %s" % (self.name, self.startDate, self.popDensity, self.data)
 
 #import data
 confData = pd.read_sql('SELECT * FROM confirmed', con=engine)
@@ -35,18 +35,21 @@ for row,col in confData.iterrows():
     stateObj = stateData()
     (a, b) = list(col.items())[0]
     stateObj.name = b
+    stateObj.data = []
 
     for colidx,val in list(col.items())[2:]:
         if (val > 0 and stateObj.startDate == None):
             stateObj.startDate = colidx
-        else:
+            stateObj.data.append(val)
+        elif (stateObj.startDate != None):
             stateObj.data.append(val)
 
     rowidx = popData.index[popData['State'] == stateObj.name]
     stateObj.popDensity = popData.loc[rowidx.values[0],'Density']
     stateArray.append(stateObj)
 
-print(stateArray)
+for x in stateArray:
+    print(x)
 
 #export data. Right now no changes are made so no need
 confData.to_sql(name = 'confirmed', con = engine, if_exists = 'replace', index = False)
