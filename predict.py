1a1521d81846a15bfdf356ce75346b9fd6260e39
Kyle L <likyle001@gmail.com>
2020-03-21 19:09:30 -0400

    updated output visual

--- predict.py
+++ predict.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 modelVars = fitData()
 
 def predict(inputDay, popDensity):
-    print(func(inputDay, modelVars[0], modelVars[1], modelVars[2]) * popDensity)
+    return func(inputDay, modelVars[0], modelVars[1], modelVars[2]) * popDensity
 
 def plot(inputDay, popDensity):
     days = []
@@ -19,8 +19,17 @@ def plot(inputDay, popDensity):
 
     for day in days:
         predY.append(predict(day,popDensity))
-        
-    plt.plot(days, predY, 'r-')
-    plt.plot([inputDay], predY[inputDay], 'bo')
 
+    plt.plot(days, predY, color='lightblue', linewidth=3)
+    plt.plot([inputDay], predY[inputDay], color='darkblue', marker='o')
+    plt.xlim(0, dayRange - 1)
+    plt.title("Covid-19 Transmission Projection")
+    plt.xlabel("Days Since First Case")
+    plt.ylabel("Number of Confirmed Cases")
+    
+    
+    label = "    Day: " + str(inputDay) + ", Cases: " + str(int(predY[inputDay]))
+    
+    plt.annotate(label, xy =(inputDay, predY[inputDay]) )
+    
     plt.show()
